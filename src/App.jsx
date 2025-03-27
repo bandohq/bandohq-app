d608420a8818dff7c069bba1ce1d5c715dde3d66
Celso Cardenas <cardenascelso9@gmail.com>
2025-03-27 16:10:38 -0600

    add connect button

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
+import { AppProvider } from "./AppProvider";
+import { ConnectButton } from "./components/ConnectButton/ConnectButton";
 import { Widget } from "./components/Widget/Widget";
 
 function App() {
   return (
-    <>
+    <AppProvider>
       <h1>Bando Widget dApp</h1>
+      <ConnectButton />
       <Widget />
-    </>
+    </AppProvider>
   );
 }
 
