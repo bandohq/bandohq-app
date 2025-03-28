eca558fe06c84cad8ee28820f689b6c02b5805cd
Celso Cardenas <cardenascelso9@gmail.com>
2025-03-27 23:10:33 -0600

    include App Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import "./App.css";
 import { Widget } from "@components/Widget/Widget";
-import CleanLayout  from "@layouts/CleanLayout";
-
+import CleanLayout from "@layouts/CleanLayout";
+import { AppProvider } from "./AppProvider";
+import { ConnectButton } from "./components/ConnectButton/ConnectButton";
 
 function App() {
   return (
-    <CleanLayout>
-      <Widget />
-    </CleanLayout>
+    <AppProvider>
+      <CleanLayout>
+        <Widget />
+      </CleanLayout>
+    </AppProvider>
   );
 }
 
