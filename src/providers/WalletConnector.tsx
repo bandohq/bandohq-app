c9980a0a813d07fe9c450bfbf06d5983d5166821
Celso Cardenas <cardenascelso9@gmail.com>
2025-03-28 13:31:44 -0600

    add wagmi automatic config for active chain

--- src/providers/WalletConnector.tsx
+++ src/providers/WalletConnector.tsx
@@ -8,55 +8,49 @@ import { createConfig, http, WagmiProvider } from "wagmi";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { injectedWallet } from "@rainbow-me/rainbowkit/wallets";
 import { BANDO_API_ROUTE } from "../utils/consts";
+import nativeTokenCatalog from "../utils/nativeTokenCatalog";
+import { transformToChainConfig } from "../utils/TransformToChainConfig";
 
 const queryClient = new QueryClient();
 
-const fetchActiveChains = async () => {
-  const response = await fetch(`${BANDO_API_ROUTE}networks/`);
-  const networks = await response.json();
-  return networks.filter((network) => network.isActive);
-};
+const connectors = connectorsForWallets(
+  [
+    {
+      groupName: "Recommended",
+      wallets: [injectedWallet],
+    },
+  ],
+  {
+    appName: "Bando Widget App",
+    projectId: "044601f65212332475a09bc14ceb3c34",
+  }
+);
 
 export const WalletConnectorProvider = ({ children }) => {
   const [config, setConfig] = useState(null);
+
+  const fetchActiveChains = async () => {
+    const response = await fetch(`${BANDO_API_ROUTE}networks/`);
+    const { data: networks } = (await response.json()) || [];
+    const activeNetworks = networks.filter((network) => network.isActive);
+    return activeNetworks;
+  };
 
   useEffect(() => {
     const setupChains = async () => {
       const activeNetworks = await fetchActiveChains();
 
-      const chainDefinitions = activeNetworks.map((network) => ({
-        id: network.chainId,
-        name: network.name,
-        nativeCurrency: network.nativeCurrency,
-        rpcUrls: {
-          default: { http: [network.rpcUrl] },
-        },
-        blockExplorers: network.blockExplorer
-          ? {
-              default: {
-                name: network.blockExplorer.name,
-                url: network.blockExplorer.url,
-              },
-            }
-          : undefined,
-      }));
-
-      const connectors = connectorsForWallets(
-        [
-          {
-            groupName: "Recommended",
-            wallets: [injectedWallet],
-          },
-        ],
-        {
-          appName: "Bando Widget App",
-          projectId: "044601f65212332475a09bc14ceb3c34",
-        }
-      );
+      const chainDefinitions = activeNetworks.map((network) => {
+        const nativeToken = nativeTokenCatalog.find(
+          (token) => token.key === network.key
+        );
+        return transformToChainConfig(network, nativeToken);
+      });
 
       const wagmiConfig = createConfig({
         connectors,
-        chains: chainDefinitions,
+        // @ts-ignore format based on viem docs
+        chains: [...chainDefinitions],
         transports: chainDefinitions.reduce((acc, chain) => {
           acc[chain.id] = http();
           return acc;
