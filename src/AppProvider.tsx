ddb952c3e244fce21450ebca654ed30f72178fac
Celso Cardenas <cardenascelso9@gmail.com>
2025-04-08 16:37:09 -0600

    add intercom to app

--- src/AppProvider.tsx
+++ src/AppProvider.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { WalletConnectorProvider } from "./providers/WalletConnector";
+import { IntercomProvider } from "./providers/IntercomProvider";
 
 export const AppProvider = ({ children }: { children: React.ReactNode }) => {
-  return <WalletConnectorProvider>{children}</WalletConnectorProvider>;
+  return (
+    <IntercomProvider>
+      <WalletConnectorProvider>{children}</WalletConnectorProvider>
+    </IntercomProvider>
+  );
 };
