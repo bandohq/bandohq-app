aaf7ea73af2f105fc27eb2f38d36165bc9d6e93b
Celso Cardenas <cardenascelso9@gmail.com>
2025-05-14 16:36:46 -0600

    fixed account button

--- src/components/ConnectButton/ConnectButton.tsx
+++ src/components/ConnectButton/ConnectButton.tsx
@@ -74,7 +74,6 @@ export const ConnectButton = () => {
                   <div style={{ display: "flex" }}>
                     <Button
                       variant="contained"
-                      onClick={openAccountModal}
                       size="small"
                       sx={{
                         fontSize: { xs: 12, sm: "1rem" },
