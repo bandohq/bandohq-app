eee54e04584f07678bbe86af6cfdf947c5dff401
Celso Cardenas <cardenascelso9@gmail.com>
2025-05-16 01:07:40 -0600

    add normal log

--- src/components/Widget/Widget.tsx
+++ src/components/Widget/Widget.tsx
@@ -14,7 +14,7 @@ export const Widget = () => {
   const { switchChain } = useSwitchChain();
 
   useEffect(() => {
-    window.parent.postMessage({ type: "log", args: "chainID: 42220" }, "*");
+    console.log("switchChain");
     switchChain({ chainId: 42220 });
   }, []);
 
