846cc76e1df683a50e67f404f323ebfb7391a4dc
Celso Cardenas <cardenascelso9@gmail.com>
2025-05-16 00:57:50 -0600

    comment

--- src/providers/WalletConnector.tsx
+++ src/providers/WalletConnector.tsx
@@ -95,7 +95,7 @@ export const WalletConnectorProvider = ({ children }) => {
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
         <RainbowKitProvider
-          initialChain={42220}
+          //initialChain={42220}
           theme={theme.palette.mode === "dark" ? midnightTheme() : lightTheme()}
         >
           {children}
