45543a0a76b558a7645427af852345b13361aacc
Celso Cardenas <cardenascelso9@gmail.com>
2025-06-30 11:56:39 -0600

    remove switch chain button for farcaster and BC

--- src/components/Widget/Widget.tsx
+++ src/components/Widget/Widget.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { BandoWidget, WidgetConfig } from "@bandohq/widget";
-import { useTheme, useMediaQuery, Button } from "@mui/material";
+import { useTheme } from "@mui/material";
 import { useConnectModal } from "@rainbow-me/rainbowkit";
-import { useSwitchChain, useChainId, useAccount } from "wagmi";
-import SyncAltIcon from "@mui/icons-material/SyncAlt";
 import { useIsFarcaster, useMiniPayDetection } from "@hooks/walletDetect";
 
 export const Widget = () => {
@@ -13,8 +11,6 @@ export const Widget = () => {
   const isMiniApp = useIsFarcaster();
   const theme = useTheme();
   const { isMiniPay } = useMiniPayDetection();
-  const { switchChain } = useSwitchChain();
-  const chainId = useChainId();
 
   const integrator = isMiniPay
     ? "opera-minipay-app"
@@ -22,14 +18,6 @@ export const Widget = () => {
     ? "farcaster-app"
     : "bando-app";
 
-  const handleSwitchChain = async () => {
-    if (chainId === 42220) {
-      await switchChain({ chainId: 8453 });
-    } else {
-      await switchChain({ chainId: 42220 });
-    }
-  };
-  
   // we are carefully opening countries on the minipay opera wallet.
   const miniPayCountries = ["US", "MX", "NG", "GH", "KE", "ZA"];
 
@@ -58,31 +46,6 @@ export const Widget = () => {
   } as Partial<WidgetConfig>;
   return (
     <>
-      {isMiniApp && (
-        <Button
-          onClick={() => handleSwitchChain()}
-          variant="contained"
-          size="small"
-          sx={{
-            backgroundColor: chainId !== 42220 ? "#FCFF51" : "#0052FE",
-            boxShadow: "none",
-            color: chainId !== 42220 ? "#000" : "#fff",
-            borderRadius: "10px",
-            fontWeight: "bold",
-            fontSize: "16px",
-            textTransform: "none",
-            width: "180px",
-            mx: "auto",
-            marginBottom: "10px",
-            "&:hover": {
-              boxShadow: "none",
-            },
-          }}
-        >
-          {chainId === 42220 ? "Switch to Base" : "Switch to Celo"}
-          <SyncAltIcon sx={{ fontSize: "20px", marginLeft: "5px" }} />
-        </Button>
-      )}
       <BandoWidget integrator={integrator} config={config} />
     </>
   );
