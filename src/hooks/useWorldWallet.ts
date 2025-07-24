19ceeb54e8f1a669451f4bc05011d0ecbb70da7e
Celso Cardenas <cardenascelso9@gmail.com>
2025-07-24 14:41:08 -0600

    return wallet auth

--- src/hooks/useWorldWallet.ts
+++ src/hooks/useWorldWallet.ts
@@ -1,12 +1,15 @@
 import { useEffect, useState } from "react";
 import { MiniKit } from "@worldcoin/minikit-js";
+import { generateUUID } from "utils/generateUUID";
 
 type WorldWallet = {
   username: string | null;
+  address: string | null;
 };
 
 export const useWorldWallet = (): WorldWallet => {
   const [username, setUsername] = useState<string | null>(null);
+  const [address, setAddress] = useState<string | null>(null);
 
   useEffect(() => {
     const run = async () => {
@@ -17,6 +20,13 @@ export const useWorldWallet = (): WorldWallet => {
       try {
         const username = MiniKit.user.username;
         setUsername(username);
+        const { finalPayload } = await MiniKit.commandsAsync.walletAuth({
+          nonce: generateUUID(),
+        });
+
+        if (finalPayload.status === "success") {
+          setAddress(finalPayload.address);
+        }
       } catch (e) {
         console.warn("walletAuth error:", e);
       }
@@ -25,5 +35,5 @@ export const useWorldWallet = (): WorldWallet => {
     run();
   }, []);
 
-  return { username };
+  return { username, address };
 };
